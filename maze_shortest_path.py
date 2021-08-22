b7c59fed0e88fa17c341033c689a31180df43c15
Rodrigo Mota <raafm@cin.ufpe.br>
2021-08-22 12:32:18 -0300

    show path in maze and written info on screen

--- maze_shortest_path.py
+++ maze_shortest_path.py
@@ -1,7 +1,7 @@
 from data_struct.queue import queue
 from graph.Maze import labirinth as maze   
 import pygame,time,random
-from graph.color import Green,Cyan,Blue,Yellow 
+from graph.color import Green,Cyan,Blue,Lime,Yellow
 
 
 pygame.init()
@@ -18,6 +18,8 @@ screen.fill((0,0,0))
 
 ROWS = len(maze)
 COLS = len(maze[0])
+
+predecessor =  list( list( ( -1 , -1 ) for _ in range(COLS) ) for _ in range(ROWS) )
 
 White = (255,255,255)
 Black = ( 0 , 0 , 0 )
@@ -43,31 +45,33 @@ def insert_queue(i,j):
 def display_maze():
     N = len(maze)
 
-
     for x in range(N):
         for y in range(N):
             pygame.draw.rect(screen ,  maze[x][y]  , ( 50 + 5*x , 50 + 5*y , 5 , 5 ) )
         pygame.display.update()
 
-    maze[0][1] = maze[0][2] = maze[0][3] = maze[0][4] = Yellow
+    #maze[0][1] = maze[0][2] = maze[0][3] = maze[0][4] = Yellow
     maze[99][95] = maze[99][96] = maze[99][97] = maze[99][98] = maze[99][99] = Green
 
     
-    time.sleep(1.5)
+    time.sleep(1)
+    
+    font = pygame.font.Font('freesansbold.ttf',15)
+    text = font.render("Entry (Entrada).",True, Cyan)                        
+    screen.blit(text,text.get_rect(center = (100,30)))
 
-    for k in range(5):
-        pygame.draw.rect(screen , maze[0][k],(50,50 + 5*k,5,5))
-        pygame.draw.rect(screen , maze[99][95+k],(50 + 5*99,50 + 5*(k+95),5,5))
+    font = pygame.font.Font('freesansbold.ttf',15)
+    text = font.render("Exit (Saida)" ,True, Green)                         
+    screen.blit(text,text.get_rect(center = (560,570)))
 
     pygame.display.update()
-
-    time.sleep(1)
-
 
 display_maze()
 
 N_layer = 1
 missing = 0
+
+found = False
 
 source = (0,0)
 current = source
@@ -93,7 +97,7 @@ while running :
     if pause:
         continue
 
-    if not see_neighbours:
+    if not see_neighbours and not found:
 
         if missing == 0:
             missing = N_layer
@@ -118,23 +122,43 @@ while running :
             screen.blit(text,text.get_rect(center = (860,50)))
             pygame.display.update()
             
-            
-            pause = True
+            found = True
             continue
+
         remove_queue(i,j)
         see_neighbours = True
         
 
-    if see_neighbours:
+    if see_neighbours and not found:
         i,j = current
-        if(       (i+1 < ROWS)             and   (maze[i+1][ j ] == Black or maze[i+1][ j ] == Green) ): insert_queue(i+1 ,  j ); Q.insert((i+1 ,  j )); N_layer += 1
-        if(        (0  <  i )              and   (maze[i-1][ j ] == Black or maze[i-1][ j ] == Green) ): insert_queue(i-1 ,  j ); Q.insert((i-1 ,  j )); N_layer += 1
-        if(       (j+1 < COLS)             and   (maze[ i ][j+1] == Black or maze[ i ][j+1] == Green) ): insert_queue( i  , j+1); Q.insert(( i  , j+1)); N_layer += 1
-        if(        (j  > 0 )               and   (maze[ i ][j-1] == Black or maze[ i ][j-1] == Green) ): insert_queue( i  , j-1); Q.insert(( i  , j-1)); N_layer += 1
-        if( (i+1 < ROWS)  and (j+1 < COLS) and   (maze[i+1][j+1] == Black or maze[i+1][j+1] == Green) ): insert_queue(i+1 , j+1); Q.insert((i+1 , j+1)); N_layer += 1
-        if( (i+1 < ROWS)  and (j > 0)      and   (maze[i+1][j-1] == Black or maze[i+1][j-1] == Green) ): insert_queue(i+1 , j-1); Q.insert((i+1 , j-1)); N_layer += 1
-        if( (0 <  i) and (j+1 < COLS)      and   (maze[i-1][j+1] == Black or maze[i-1][j+1] == Green) ): insert_queue(i-1 , j+1); Q.insert((i-1 , j+1)); N_layer += 1
+        if(       (i+1 < ROWS)             and   (maze[i+1][ j ] == Black or maze[i+1][ j ] == Green) ): insert_queue(i+1 ,  j ); Q.insert((i+1 ,  j )); N_layer += 1 ; predecessor[i+1][ j ] = ( i , j )
+        if(        (0  <  i )              and   (maze[i-1][ j ] == Black or maze[i-1][ j ] == Green) ): insert_queue(i-1 ,  j ); Q.insert((i-1 ,  j )); N_layer += 1 ; predecessor[i-1][ j ] = ( i , j )
+        if(       (j+1 < COLS)             and   (maze[ i ][j+1] == Black or maze[ i ][j+1] == Green) ): insert_queue( i  , j+1); Q.insert(( i  , j+1)); N_layer += 1 ; predecessor[ i ][j+1] = ( i , j )
+        if(        (j  > 0 )               and   (maze[ i ][j-1] == Black or maze[ i ][j-1] == Green) ): insert_queue( i  , j-1); Q.insert(( i  , j-1)); N_layer += 1 ; predecessor[ i ][j-1] = ( i , j )
+        if( (i+1 < ROWS)  and (j+1 < COLS) and   (maze[i+1][j+1] == Black or maze[i+1][j+1] == Green) ): insert_queue(i+1 , j+1); Q.insert((i+1 , j+1)); N_layer += 1 ; predecessor[i+1][j+1] = ( i , j )
+        if( (i+1 < ROWS)  and (j > 0)      and   (maze[i+1][j-1] == Black or maze[i+1][j-1] == Green) ): insert_queue(i+1 , j-1); Q.insert((i+1 , j-1)); N_layer += 1 ; predecessor[i+1][j-1] = ( i , j )
+        if( (0 <  i) and (j+1 < COLS)      and   (maze[i-1][j+1] == Black or maze[i-1][j+1] == Green) ): insert_queue(i-1 , j+1); Q.insert((i-1 , j+1)); N_layer += 1 ; predecessor[i-1][j+1] = ( i , j )
         
         see_neighbours = False
 
         time.sleep(0.001)
+
+    if found:
+        time.sleep(0.01)
+        (x,y) = current
+        if( current != source ):
+            pygame.draw.rect(screen ,  Green  , ( 50 + 5*x , 50 + 5*y , 5 , 5 ) )
+            x,y = predecessor[x][y]
+            pygame.draw.rect(screen ,  Lime  , ( 50 + 5*x , 50 + 5*y , 5 , 5 ) )
+            pygame.display.update()
+
+            current = (x,y)
+
+        if current == source:
+            pygame.draw.rect(screen ,  Green  , ( 50 + 5*x , 50 + 5*y , 5 , 5 ) )
+            pygame.display.update()
+            pause = True
+            found = False
+            continue
+
+        
