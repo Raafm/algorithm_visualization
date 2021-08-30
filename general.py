d10dc6715e264c6a86bb900903ffd76387e89f24
Rodrigo Mota <raafm@cin.ufpe.br>
2021-08-30 04:57:07 -0300

    usefull functions

---
+++ general.py
@@ -0,0 +1,83 @@
+import pygame,time
+from math import hypot
+
+def distance(node1,node2):
+    return hypot(node1[0]-node2[0], node1[1]-node2[1])
+
+
+def show_weight(screen,peso,position,font,peso_color = (0,255,255),radius = 10):
+    text = font.render(str(peso),True,peso_color,radius)
+    screen.blit(text,position)
+
+
+def median_point(p1,p2):
+    return ((p1[0] + p2[0])/2, (p1[1] + p2[1])/2)
+
+
+def memorize(screen,node_center,radius=10,Time = 0.1,show=True):
+    pygame.draw.circle(screen,(255,0,0),node_center,radius)
+
+    if show:
+        pygame.display.update()
+        time.sleep(Time)
+
+
+def visit(screen,node_center,visit_color = (0,255,0),radius = 10,show=True,Time = 0.1):
+
+    pygame.draw.circle(screen,visit_color,node_center,radius)
+
+    if show:
+        pygame.display.update()
+        time.sleep(Time)
+
+
+
+def visited(screen,node_center,radius = 10,show=True,Time = 0.1):
+    Cyan 	     =	    (0,255,255)
+    pygame.draw.circle(screen, Cyan , node_center , radius)
+
+    
+
+    if show:
+        pygame.display.update()
+        time.sleep(Time)
+
+
+
+def mark(screen,node_center,color,radius):
+
+    pygame.draw.circle(screen, color, node_center , radius)
+
+
+
+# name of the algorithm
+def top_info(screen,info_text,center_position = (400,30),size = 30):
+    font = pygame.font.Font('freesansbold.ttf',size)
+    text = font.render(info_text,True,(150,150,150))                        
+    screen.blit(text,text.get_rect(center = center_position))
+    pygame.display.update()
+
+
+
+def informative_nodes(screen,
+                    memory_color  = (255, 0 , 0 ), memory_text ="memorized nodes",
+                    visited_color = ( 0 ,255,255), visited_text ="visited nodes",
+                    current_color = ( 0 ,255, 0 ), current_text = "current"):
+
+    font = pygame.font.Font('freesansbold.ttf',15)
+    pygame.draw.circle(screen,memory_color, (850,200),7)
+    text = font.render(memory_text,True,memory_color)
+    screen.blit(text,text.get_rect(center = (915,200)))                             
+
+
+    font = pygame.font.Font('freesansbold.ttf',15)
+
+
+    pygame.draw.circle(screen,  (255,255,255), (850,150) , 7)
+    pygame.draw.circle(screen,visited_color,(850,150),5)
+    text = font.render(visited_text,True,visited_color)                               # informative node       
+    screen.blit(text,text.get_rect(center = (925,150)))
+
+    pygame.draw.circle(screen,current_color,(850,175),7)
+    text = font.render(current_text,True,current_color)                            # informative node   
+    screen.blit(text,text.get_rect(center = (925,175)))
