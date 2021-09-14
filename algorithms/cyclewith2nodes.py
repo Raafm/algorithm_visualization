ef0a9988952ed8e124be08c5ba60a9973e2b8878
Rodrigo Mota <raafm@cin.ufpe.br>
2021-09-14 01:36:05 -0300

    algorithm fixed

--- algorithms/cyclewith2nodes.py
+++ algorithms/cyclewith2nodes.py
@@ -55,8 +55,8 @@ def mark(screen,node_center,color,radius=8,show=True,Time = 0.1):
         pygame.display.update()
         time.sleep(Time)
 
-def cycleWith2Nodes(graph,node_position,s = 0,t = 1):
-    from algorithms.colors import Dark_red,Flame,Cyan,White,Blue,royalblue,Black,Springgreen,Green
+def cycleWith2Nodes(graph,node_position,s = 0,t = 1,steps_mode = False):
+    from algorithms.colors import Dark_red,Flame,Cyan,White,Blue,royalblue,Black,Springgreen,Green,Lime
     from algorithms.data_struct.queue import queue
     from algorithms.data_struct.stack import stack
 
@@ -68,246 +68,152 @@ def cycleWith2Nodes(graph,node_position,s = 0,t = 1):
 
     screen.fill((0,0,0))    
 
+    N = len(graph) #number of nodes in the graph
 
-    N = len(graph)
-    #tranforming adj list in a matrix graph
-    matrix_graph = list(list(False for _ in range(N)) for _ in range(N))
-    input_graph = graph
-
-
+    #print edges
     for node in range(N):
         for neighbour in graph[node]:
-            pygame.draw.line(screen,White,node_position[node],node_position[neighbour],2)
-            matrix_graph[neighbour][node] = matrix_graph[node][neighbour] = True 
-
-
+            pygame.draw.line(screen,White,node_position[node],node_position[neighbour],1)
+    #print nodes
     for node in range(N):
-        pygame.draw.circle(screen,Blue,node_position[node],8)
+        mark(screen,node_position[node],Blue,10,False)
 
     pygame.display.update()
 
-
-
-    graph = matrix_graph
-
-
-
-    Q = queue()
-    S = stack()
-
-
-    cur = s
-    seen  = list( False for _ in range(N))
-    parent   = list(  -1   for _ in range(N))
-    flow_to = list(  -1   for _ in range(N))
+    for _ in range(N):
+        graph.append([])
     
+    for node in range(N):
+        if node != s and node != t:
+            for neighbour in graph[node]:
+                graph[node+N].append(neighbour)
+                graph[node] = [node+N]
+    
+    temp_list = graph[t]
+    graph[t] = []
+    for neighbour in temp_list:
+        graph[t].append(neighbour+N)
 
-
-    seen[s] = True
+    predecessor = list( -1 for _ in range(2*N))
+    flow_to = list( -1 for _ in range(2*N))
+    Q = queue()
     Q.insert(s)
 
-    bfs1 = True
-    found1 = False
-    bfs2 = False
-    found2 = False
-    update_path1 = False
-    update_path2 = False
-    drawing = False
-    pause = False
-    while True :
+    bfs = True
+    path = False
+    cycle = False
+    first_iteration = True
 
-        # pygame stuff:
+    pause = False
+    while True:
+
+        
+            # pygame stuff:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()                   #exit pygame,
                 quit()                          #exit() program
 
-                
+
+            
             if event.type == pygame.KEYDOWN:        
                 if event.key == pygame.K_SPACE:     #press breakspace to pause or play
                     pause = not pause   
                     time.sleep(0.2)
-            
+        
         if pause:
             continue
+        
+        if steps_mode: #pause every step of the algorithm
+            pause = True  #when this iteration ends,the animation is paused
 
-        if bfs1:
-            if Q.not_empty():
-                cur = Q.pop()
-                visit(screen,node_position[cur],Time = 0.01)
-            else:
-                bfs1 = False
-                update_path1 = True
-                time.sleep(1)
-                continue
 
-            if cur == t:
-                while Q.not_empty():Q.pop()
-                found1 = True
-                continue
-
-            for neighbour in  range(N):
-                if (not seen[neighbour]) and (graph[cur][neighbour]):
-                    seen[neighbour] = True
-                    parent[neighbour]  = cur
+        if bfs:
+            cur = Q.pop()
+            visit(screen,node_position[cur%N],Time = 0.01)
+            for neighbour in graph[cur]:
+                if predecessor[neighbour] < 0: # has not seen the node
                     Q.insert(neighbour)
-                    memorize(screen,node_position[neighbour],Time = 0.01)
-            
-            visited(screen,node_position[cur])
-                    
-                
-
-        elif update_path1:
-            if not found1:
-                update_path1 = False
-                pause = True
-                continue
-
-            if not drawing:
-                pygame.draw.rect(screen,Black,(0,0,1000,1000))
-                for node in range(N):
-                    for neighbour in input_graph[node]:
-                        pygame.draw.line(screen,White,node_position[node],node_position[neighbour],2)
-
-                for node in range(N):
-                    pygame.draw.circle(screen,Blue,node_position[node],8)
-
-
-                cur = t
-                while s != cur:
-                    pred = parent[cur]
-                    S.insert((pred,cur)) 
-                    graph[pred][cur] = False
-                    flow_to[cur] = pred
-                    cur = pred
-                drawing = True      
-            else:
-                if S.not_empty():
-                    node1,node2 = S.pop()
-                    
-                    arrow(screen,node_position[node1], node_position[node2],Springgreen,Green	)
-                    time.sleep(0.01)
-
-                else:
-                    drawing = False
-                    bfs2 = True
-                    update_path1 = False
-                    seen = list(False for _ in range(N))
-                    seen[s] = True
-                    first_flow_to_t = flow_to[t]
-                    flow_to[t] = -1
-                    Q.insert(s)
-                    time.sleep(1)
-        
-
-
-        elif bfs2:
-            
-            if cur ==  t:
-                while Q.not_empty():Q.pop()
-                found2 = True
-                           
-
-            if Q.not_empty():
-                cur = Q.pop()
-                visit(screen,node_position[cur],Time = 0.01)
-            else:
-                bfs2 = False
-                update_path2 = True
-                time.sleep(1)
-                continue
-
-
- 
-
-            #if flow_to[cur] > 0:
-            #    pred = flow_to[cur]
-            #    
-#
-#
-            #    if not seen[pred]:
-            #        Q.insert(pred)
-            #        parent[cur]  = pred
-            #        memorize(screen,node_position[cur],Time = 0.01)
-            #    
-            
-            for neighbour in  range(N):
-                if (not seen[neighbour]) and (graph[cur][neighbour] > 0):
-                    if neighbour == t:
-                        parent[t] = cur
-                        cur= t
-                        break
-                        
-
-                    if flow_to[neighbour] > 0:
-                        pred = flow_to[neighbour]
-                        if not seen[pred]:
-                            seen[pred] = True
-                            parent[pred] = neighbour
-                            parent[neighbour] = cur
-                            Q.insert(pred)
-                            memorize(screen,node_position[pred],Time = 0.01)
-                    else:  
-                        seen[neighbour] = True
-                        parent[neighbour]  = cur
-                        Q.insert(neighbour)
-                        memorize(screen,node_position[neighbour],Time = 0.01)
-
-            visited(screen,node_position[cur])
-            time.sleep(0.01)
-
-        elif update_path2:
-            print(1)
-            if not found2:
-                update_path2 = False
-                pause = True
-                continue
-            print(2)
-            if not drawing:
-                cur = t
-                while s != cur:
-                    pred = parent[cur]
-                    if flow_to[pred] != cur:
-                        flow_to[cur] = pred
-                    S.insert((pred,cur))
-                    cur = pred 
-
-                drawing = True      
-            else:
-                if S.not_empty():
-                    node1,node2 = S.pop()
-                    arrow(screen,node_position[node1], node_position[node2],royalblue,royalblue)
-                    time.sleep(0.3)
-
-                else:
-                    drawing = False
-                    seen = list(False for _ in range(N))
-                    seen[s] = True
-                    Q.insert(s)
-                    update_path2 = False
+                    predecessor[neighbour] = cur
+                    memorize(screen,node_position[neighbour%N],Time = 0.01)
+                    if cur == t:
+                        bfs = False
+                        path = True
+            visited(screen,node_position[cur%N],Time = 0.01)
 
 
 
-        
-        else:
-            time.sleep(1)
+        elif path:
             cur = t
+            S = stack()
+            while cur != s:
+                pred = predecessor[cur]
+                S.insert((pred,cur)) # insert to make a good animation latter
+                #reverse edge
+                graph[cur].append(pred) 
+                graph[pred].remove(cur)
+                
+                if flow_to[pred] !=  cur: flow_to[cur] = pred
+                
+                cur = pred
+
+            while S.not_empty(): # animation showing path
+                node1,node2 = S.pop()
+                arrow(screen,node_position[node1%N],node_position[node2%N])
+                time.sleep(0.3)
+
+            if first_iteration == True:
+                first_flow_to_t = flow_to[t]
+                flow_to[t] = -1
+                predecessor = list(-1 for _ in range(2*N))
+                Q = queue()
+                Q.insert(s)
+
+                #print edges
+                for node in range(N):
+                    for neighbour in graph[node]:
+                        pygame.draw.line(screen,White,node_position[node%N],node_position[neighbour%N],1)
+                #print nodes
+                for node in range(N):
+                    mark(screen,node_position[node%N],Blue,10,False)
+
+                pygame.display.update()
+
+
+                first_iteration = False
+                path = False
+                bfs = True
+            else:
+                cycle = True
+                path = False
+        
+
+
+        elif cycle:
+            cur = t
+            S = stack()
             while cur != s:
                 pred = flow_to[cur]
                 S.insert((pred,cur))
                 cur = pred
-
+           
             while S.not_empty():
                 node1,node2 = S.pop()
-                arrow(screen,node_position[node1],node_position[node2],Flame,Dark_red,10,7)
+                arrow(screen,node_position[node1%N],node_position[node2%N],Flame,Dark_red,6,4)
                 time.sleep(0.3)
-
+            
             cur = first_flow_to_t
+            arrow(screen,node_position[t],node_position[cur%N])
             while cur != s:
-                pred = flow_to[cur]
-                arrow(screen,node_position[cur],node_position[pred],Flame,Dark_red,10,7)
+                node2,node1 = flow_to[cur],cur
+                arrow(screen,node_position[node1%N],node_position[node2%N],Flame,Dark_red,6,4)
+                cur = flow_to[cur]
                 time.sleep(0.3)
-                cur = pred
-
+            
+            cycle = False
             pause = True
-            continue
+            
+
+            
+            
+                
